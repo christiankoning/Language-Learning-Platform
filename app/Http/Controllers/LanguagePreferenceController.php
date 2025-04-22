098cc4bce73a384305304a392c6f83ff2f0d13fb
ChristianKoning <S1170814@student.windesheim.nl>
2025-04-22 19:06:39 +0200

    Add controller for changing language preference

---
+++ app/Http/Controllers/LanguagePreferenceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LanguagePreferenceController extends Controller
+{
+    public function update(Request $request)
+    {
+        $request->validate([
+            'preferred_language' => 'required|in:en,nl'
+        ]);
+
+        $user = auth()->user();
+        $user->preferred_language = $request->preferred_language;
+        $user->save();
+
+        return response()->json(['status' => 'ok']);
+    }
+}
